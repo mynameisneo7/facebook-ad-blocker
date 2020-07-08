ea6022824022c2423e9b41e937827d290432703a
Gea-Suan Lin <gslin@gslin.org>
2020-07-08 15:10:48 +0800

    Import.

---
+++ facebook-ad-blocker.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Facebook ad blocker
+// @namespace    https://wiki.gslin.org/wiki/FacebookAdBlocker
+// @version      0.20200703.1
+// @description  Remove all ad from Facebook
+// @author       Gea-Suan Lin <gslin@gslin.org>
+// @match        https://www.facebook.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let observer = new MutationObserver(() => {
+        document.querySelectorAll('div[data-pagelet^="FeedUnit_"]').forEach(div => {
+            div.querySelectorAll('b[style="display: none;"]').forEach(b => {
+                let span = b.closest('span');
+                if (span.innerText.startsWith('Sponsored')) {
+                    console.info('Remove a Facebook ad element.');
+                    div.remove();
+                }
+            });
+        });
+    });
+
+    observer.observe(document.documentElement, {
+        attributes: false,
+        childList: true,
+        subtree: true,
+    });
+})();
