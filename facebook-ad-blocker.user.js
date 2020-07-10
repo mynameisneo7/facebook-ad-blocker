9df6750d6962c7d54666ecaeaefcf38fe22ce017
Gea-Suan Lin <gslin@gslin.org>
2020-07-10 15:28:30 +0800

    Add another rule to filter ad.

--- facebook-ad-blocker.user.js
+++ facebook-ad-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook ad blocker
 // @namespace    https://wiki.gslin.org/wiki/FacebookAdBlocker
-// @version      0.20200703.2
+// @version      0.20200710.0
 // @description  Remove all ad from Facebook
 // @author       Gea-Suan Lin <gslin@gslin.org>
 // @match        https://www.facebook.com/*
@@ -14,11 +14,20 @@
 
     let observer = new MutationObserver(() => {
         document.querySelectorAll('div[data-pagelet^="FeedUnit_"]').forEach(div => {
+            div.querySelectorAll('span').forEach(span => {
+                if (span.innerText.startsWith('Sponsored')) {
+                    console.info('Remove a Facebook ad element.');
+                    div.remove();
+                    return;
+                }
+            });
+
             div.querySelectorAll('b[style="display: none;"]').forEach(b => {
                 let span = b.closest('span');
                 if (span.innerText.startsWith('Sponsored')) {
                     console.info('Remove a Facebook ad element.');
                     div.remove();
+                    return;
                 }
             });
         });
