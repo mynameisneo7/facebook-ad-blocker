1843624eb9ffaf6103ce03f024b04e738a657300
Gea-Suan Lin <gslin@gslin.org>
2020-08-10 01:34:10 +0800

    Add one more pattern.

--- facebook-ad-blocker.user.js
+++ facebook-ad-blocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook ad blocker
 // @namespace    https://wiki.gslin.org/wiki/FacebookAdBlocker
-// @version      0.20200710.1
+// @version      0.20200810.0
 // @description  Remove all ad from Facebook
 // @author       Gea-Suan Lin <gslin@gslin.org>
 // @match        https://www.facebook.com/*
@@ -14,6 +14,13 @@
 
     let observer = new MutationObserver(() => {
         document.querySelectorAll('div[data-pagelet^="FeedUnit_"]').forEach(div => {
+            // aria-label="Sponsored"
+            div.querySelectorAll('div[aria-label="Sponsored"]').forEach(span => {
+                console.info('Remove a Facebook ad element by aria-label="Sponsored".');
+                div.remove();
+                return;
+            });
+
             div.querySelectorAll('span').forEach(span => {
                 if (span.innerText.startsWith('Sponsored')) {
                     console.info('Remove a Facebook ad element by starting with "Sponsored".');
